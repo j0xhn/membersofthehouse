4412e89147abebc03681fc8541b0854c7ec928ee
John Storey <jstorey@wayfair.com>
2019-09-22 07:21:05 -0400

    restructure

---
+++ build/precache-manifest.99b4214da297c4fe4b2228d6f62ce57d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7b75dbcad9b38f67f28012bc69d48ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93b87cb3b0d9e7cc0b69",
+    "url": "/static/css/main.5ccd3ebf.chunk.css"
+  },
+  {
+    "revision": "16ffd31b8ba994b5cedf",
+    "url": "/static/js/2.3933f565.chunk.js"
+  },
+  {
+    "revision": "93b87cb3b0d9e7cc0b69",
+    "url": "/static/js/main.b19c1fe9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79108a5b8f147e9b92838696c3abc0d8",
+    "url": "/static/media/paella.79108a5b.png"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import './App.css';
+import './Shorthand.css';
+import Vote from './routes/vote'
+import Landing from './routes/landing'
+import GlobalWrapper from './stores/global'
+import Toast from './components/Toast'
+
+function App() {
+    return (
+    <GlobalWrapper>
+      <Toast>
+        <Router>
+          <Route path="/vote/:id" component={Vote} />
+          <Route exact path="/" component={Landing} />
+        </Router>
+      </Toast>
+    </GlobalWrapper>
+  );
+}
+
+export default App;
